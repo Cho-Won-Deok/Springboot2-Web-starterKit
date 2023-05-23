d3e5d49aec406c14b5101f4b12d5cb4c9afd01b6
조원덕 <equip36@nexmotion.co.kr>
2023-05-23 21:21:40 +0900

    promise 테스트 코드 추가

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,3 +1,32 @@
+function datData (){
+    return new Promise(function(resolve, reject) {
+        let data = {
+            'result':'ok',
+            'data' : [
+                {
+                    "title": "제목1",
+                    "content": "내용1"
+                },
+                {
+                    "title": "제목2",
+                    "content": "내용2"
+                },
+                {
+                    "title": "제목3",
+                    "content": "내용33"
+                }
+            ]
+
+        };
+
+        setTimeout(() => {
+            resolve(data);
+        }, 1000);
+        
+    });
+}
+
+
 var app = angular.module('testApp', []);
 
 app.controller('testController', function($scope, $http) {
@@ -5,6 +34,9 @@ app.controller('testController', function($scope, $http) {
 
     var init = function() {
         ajax();
+        datData().then(function(result) {
+            console.log(result);
+        });
     };
 
     var ajax = function() {
